1e44063dcd344be6cf26f79357f7527895fdd4cf
Roberto Hernández Tamayo <hetr@praxis.com.mx>
2016-07-13 17:38:15 -0500

    Login
    -Se realizaron los configuraciones necesarias para permitir el logueo por medio de una cuenta de Facebook

--- app/src/main/java/edu/galileo/android/rifamania/RifaMainActivity.java
+++ app/src/main/java/edu/galileo/android/rifamania/RifaMainActivity.java
@@ -3,11 +3,11 @@ package edu.galileo.android.rifamania;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class RifaMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_rifa_main);
     }
 }
